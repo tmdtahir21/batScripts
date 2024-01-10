2ce977dba8c9feaa4500d59ab0fd8ad5b5cda7ae
MOHAMMED TAHIR <79696547+tmdtahir21@users.noreply.github.com>
2024-01-10 10:33:40 +0530

    Bat Scripts Added

---
+++ folder.bat
@@ -0,0 +1,1 @@
+md Bhubaneswar Chennai Coimbatore Hubli Hyderabad Kochi Luhari Mangalore Raipur Vijayawada
---
+++ zip.bat
@@ -0,0 +1,4 @@
+color 02
+for /d %%x in (*) do "C:\Program Files\7-Zip\7z.exe" a "%%x.zip" "%%x\"
+
+pause 	
